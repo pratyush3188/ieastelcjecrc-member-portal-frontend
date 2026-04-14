18106bed6138a4b935dec62f3d7dc816645e5791
VikramS04 <vikramsaini7723@gmail.com>
2026-04-14 15:52:13 +0530

    changes to the api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,4 @@
-// src/utils/api.js
-export const API_BASE_URL = 'http://localhost:5000';
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 // Added this: Helper to get the token quickly
 export const getAuthToken = () => {
